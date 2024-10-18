45c6c767844ac6192566ee24646b29323fd7bb04
Buertey Accam <manreg814@gmail.com>
2024-10-18 09:47:37 -0500

    update

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Homefooter from "./Onboarding/OnboardingA/Avatar/Homefooter";
+import NAvbar from "./Onboarding/OnboardingA/Avatar/NAvbar";
 
 const Homepage = () => {
   return (
     <>
-      <div>
+      <div className="homepage">
+        <div className="">
+          <NAvbar />
+        </div>
         Homepage
         <div className="">
           <Homefooter />
---
+++ frontend/src/pages/Onboarding/OnboardingA/Avatar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const NAvbar = () => {
+  return (
+    <div className="navbar-container">
+      <img src="" alt="user-image" className="navbar-img" />
+      <div className="user-details">
+        <p className="user-name">
+          Hello, <span className="name-span">telegram</span>
+        </p>
+        <h3>Bubu Manuel</h3>
+      </div>
+    </div>
+  );
+};
+
+export default NAvbar;
