dc4a19c269b5e3e5a21a88e108377cb4992de0eb
Buertey Accam <manreg814@gmail.com>
2024-10-18 03:21:38 -0500

    update

--- frontend/src/pages/Onboarding/OnboardingA/Avatar/Homefooter.jsx
+++ frontend/src/pages/Onboarding/OnboardingA/Avatar/Homefooter.jsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import { CgProfile } from "react-icons/cg";
+import { TiChartBarOutline } from "react-icons/ti";
+import { HiClipboardCheck } from "react-icons/hi";
+import { GiProgression } from "react-icons/gi";
+import { RiHome5Fill } from "react-icons/ri";
+import React from "react";
 
 const Homefooter = () => {
   return (
-    <div>Homefooter</div>
-  )
-}
+    <div className="footer">
+      {/* Home icon */}
+      <div className="home">
+        <div className="home-icon">
+          <RiHome5Fill />
+        </div>
+        <p className="home-text">Home</p>
+      </div>
+      {/* Track progress icon */}
+      <div className="home">
+        <div className="home-icon">
+          <TiChartBarOutline />
+        </div>
+        <p className="track-text">Track progress</p>
+      </div>
+      {/* Module icon */}
+      <div className="home">
+        <div className="home-icon">
+          <HiClipboardCheck />
+        </div>
+        <p className="home-text">Module</p>
+      </div>
+      {/* Profile icon */}
+      <div className="home">
+        <div className="home-icon">
+          <CgProfile />
+        </div>
+        <p className="home-text">Profile</p>
+      </div>
+    </div>
+  );
+};
 
 export default Homefooter;
